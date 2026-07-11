c5360df1ae42c895220e2402093c378363c850b0
Aleksandar <cicadrazo47@gmail.com>
2026-07-11 16:17:38 +0200

    Refactor code structure for better future handling

--- movie-details-page.js
+++ movie-details-page.js
@@ -1,60 +1,113 @@
-const apiKey = '2c3f792a'
-//https://www.omdbapi.com/?apikey=${apiKey}&t=${title}
-//const TmdbApiKey = 'Your api key'
 
+const apiKey = '2c3f792a';
+
+// https://www.omdbapi.com/?apikey=${apiKey}&t=${title}
+// const TmdbApiKey = 'Your api key'
+
+
+// ====================== URL PARAMS ======================
 
 const params = new URLSearchParams(window.location.search);
+
 const movieId = params.get('id');
+
+
+// ====================== DOM ==============================
+
 const divElement = document.querySelector('#movie-details');
 
+
+// ====================== FETCH MOVIE DATA =================
+
 fetch(`https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${TmdbApiKey}`)
-.then(res => res.json())
+
+    .then(res => res.json())
+
     .then(data => {
 
         const trailerKey = data.results[0].key;
 
+
         return fetch(`https://www.omdbapi.com/?apikey=${apiKey}&i=${movieId}`)
+
             .then(response => response.json())
+
             .then(movieData => {
+
                 divElement.innerHTML = `
- <div class="movieDetailsCard">
 
-    <div class="mediaSection">
-        <img class="moviePoster"
-             src="${movieData.Poster}"
-             alt="Movie Poster">
+                    <div class="movieDetailsCard">
 
-        <iframe
-            class="movieTrailer"
-            src="https://www.youtube.com/embed/${trailerKey}">
-        </iframe>
-    </div>
+                        <div class="mediaSection">
 
-    <div class="movieInfo">
-
-        <h2>${movieData.Title}</h2>
-
-        <div class="movieMeta">
-            <p>⭐ IMDb: ${movieData.imdbRating}</p>
-            <p>📅 ${movieData.Year}</p>
-            <p>🎬 ${movieData.Genre}</p>
-            <p>⏱ ${movieData.Runtime}</p>
-            <p>🎥 ${movieData.Director}</p>
-        </div>
-
-        <div class="plot">
-            <h3>Plot</h3>
-            <p>${movieData.Plot}</p>
-        </div>
-
-    </div>
-
-</div>
-   
-    `
-            })
-    })
+                            <img 
+                                class="moviePoster"
+                                src="${movieData.Poster}"
+                                alt="Movie Poster"
+                            >
 
 
+                            <iframe
+                                class="movieTrailer"
+                                src="https://www.youtube.com/embed/${trailerKey}">
+                            </iframe>
 
+                        </div>
+
+
+                        <div class="movieInfo">
+
+                            <h2>
+                                ${movieData.Title}
+                            </h2>
+
+
+                            <div class="movieMeta">
+
+                                <p>
+                                    ⭐ IMDb: ${movieData.imdbRating}
+                                </p>
+
+                                <p>
+                                    📅 ${movieData.Year}
+                                </p>
+
+                                <p>
+                                    🎬 ${movieData.Genre}
+                                </p>
+
+                                <p>
+                                    ⏱ ${movieData.Runtime}
+                                </p>
+
+                                <p>
+                                    🎥 ${movieData.Director}
+                                </p>
+
+                            </div>
+
+
+                            <div class="plot">
+
+                                <h3>
+                                    Plot
+                                </h3>
+
+                                <p>
+                                    ${movieData.Plot}
+                                </p>
+
+                            </div>
+
+
+                        </div>
+
+
+                    </div>
+
+                `;
+
+            });
+
+    });
 
